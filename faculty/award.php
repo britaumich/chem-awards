1f06064a6492bc88107aba5b66bdecb2caeaeaf2
Margarita Barvinok <brita@umich.edu>
2020-01-13 11:38:56 -0500

    Change link to dbConnect to /../support/

--- faculty/award.php
+++ faculty/award.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <?php
-require_once('../dbConnect.inc');
+require_once($_SERVER["DOCUMENT_ROOT"] . '/../support/awards_dbConnect.inc');
 require_once('nav.php');
 		
 $award_id = check_input($conn, $_REQUEST[award_id]);
