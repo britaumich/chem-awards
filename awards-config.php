f5213d941798b65abe266708c4a4d1c135cacbab
Margarita Barvinok <56089936+britaumich@users.noreply.github.com>
2021-01-05 17:35:42 -0500

    Update awards-config.php

--- awards-config.php
+++ awards-config.php
@@ -22,11 +22,11 @@ $pass = getenv('db_password', true) ?: getenv('db_password');
 $database = getenv('db_name', true) ?: getenv('db_name');
 
 echo $user;
-echo ("br");
+echo ("<br>");
 echo $server;
-echo ("br");
+echo ("<br>");
 echo $database;
-echo ("br");
+echo ("<br>");
 exit;
 
 $conn = mysqli_connect($server, $user, $pass) or die("couldn't connect");
