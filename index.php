ac2cde77734ea7db0dcbbd56d1c47d06f0642342
Margarita Barvinok <brita@umich.edu>
2021-03-25 17:40:23 -0400

    Add include for RSA.php

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 include('awards-config.php');
 $path = '/opt/app-root/src/vendor/phpseclib/phpseclib/phpseclib';
 set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+include('/opt/app-root/src/vendor/phpseclib/phpseclib/phpseclib/Crypt/RSA.php');
 // authentication
 require __DIR__ . '/vendor/autoload.php';
 
