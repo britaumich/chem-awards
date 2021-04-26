ea2eb2c603057a3eaeb99be3a435cb9dd9b1df27
Margarita Barvinok <56089936+britaumich@users.noreply.github.com>
2021-04-26 09:25:56 -0400

    Update login.php

--- login.php
+++ login.php
@@ -6,7 +6,12 @@ use Jumbojett\OpenIDConnectClient;
 include('awards-config.php');
 include('ldap.inc');
 
+$issuer = 'https://shibboleth.umich.edu';
+$cid = '677abbb6-ba16-4c95-b8a8-84760247592d';
+$secret = '7867e424-5bff-44de-8951-1350b4a59a1f';
+
 $oidc = new Jumbojett\OpenIDConnectClient($issuer, $cid, $secret);
+$oidc->addScope(array('openid profile email edumember'));
 $oidc->setCertPath('/opt/app-root/src/um-certs2020.pem');
 $oidc->authenticate();
 $oidc->requestUserInfo('sub');
@@ -17,5 +22,8 @@ echo $uniqname;
 $person = ldap_person($uniqname);
 echo "<br><br>person";
 var_export($person);
+
+echo "<br><br>oidc: <br>";
+var_export($oidc);
 
 ?>
